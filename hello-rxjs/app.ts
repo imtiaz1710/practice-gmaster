b4f212d788570003b8319bba3ae83f8226b49a5f
imtiaz <imtiaz.mehedi.iubat@gmail.com>
2022-04-06 16:35:21 +0600

    commit removed 1

--- hello-rxjs/app.ts
+++ hello-rxjs/app.ts
@@ -1,28 +1,6 @@
 import { concat, interval, merge } from "rxjs";
 import { skip, take, map } from "rxjs/operators";
 
-// console.log("starting countdown");
-// console.log("3");
-// setTimeout(() => {
-//     console.log("2");
-//     setTimeout(() => {
-//         console.log("1");
-//         setTimeout(() => {
-//             console.log("0");
-//         }, 1000);
-//     }, 1000);
-// }, 1000);
-
-// let counter = 4;
-
-// const interval = setInterval(() => {
-//     console.log(counter--);
-//     if (counter < 0) clearInterval(interval);
-// }, 1000);
-
-// interval(1000)
-//     .pipe(take(3), map(n => 2 - n))
-//     .subscribe((n) => console.log(n));
 
 merge(
     interval(500).pipe(
